9bd91bc91419935a1b3c5090f406ea33ceb3bf32
adodeme <adodemeflodias@gmail.com>
2025-05-07 18:50:03 +0100

    Add files via upload

---
+++ MON TRAVAIL/exo.py
@@ -0,0 +1,25 @@
+
+#for i in range {0; 1000} :
+
+ #   nmbre=print("Entrer un nombre")
+# O={0;9}
+
+
+
+        
+#for i in range {0,9}:
+#        print(i)
+  
+
+
+# rayon
+
+
+
+
+R=int(input("Veuillez entrer le rayon du cercle :"))
+print("Détermination de l'aire du cercle :")
+print("A=22/7 x RxR")
+print("A=22/7 x",R*R)
+print("A="22/7,*,R*R)
+print("L'aire de ce cercle est:",A)
